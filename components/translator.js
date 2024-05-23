2de413d4373873287253e6de7d424645a429053e
ShivrajSDev <166504815+ShivrajSDev@users.noreply.github.com>
2024-05-23 20:16:26 +0100

    feat: Add API and domain logic

--- components/translator.js
+++ components/translator.js
@@ -3,8 +3,61 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
-class Translator {
+const americanOnlyWords = Object.keys(americanOnly);
+const britishFromAmericanOnly = Object.values(americanOnly);
+const britishOnlyWords = Object.keys(britishOnly);
+const americanFromBritishOnly = Object.values(britishOnly);
+const americanSpelling = Object.keys(americanToBritishSpelling);
+const britishSpelling = Object.values(americanToBritishSpelling);
+const americanTitles = Object.keys(americanToBritishTitles);
+const britishTitles = Object.values(americanToBritishTitles);
 
+class Translator {
+    translateWord(text, wordsFrom, wordsTo) {
+        wordsFrom.forEach((word, i) => {
+            let regex = new RegExp(`(?<=^|[.'"\\s])${word}(?=[.'"\\s]|$)`, 'gi');
+            let replacingWord = wordsTo[i];
+            text = text.replaceAll(regex, `<span class="highlight">${replacingWord}</span>`);
+        });
+        
+        return text;
+    };
+
+    translateTitle(text, titlesFrom, titlesTo) {
+        titlesFrom.forEach((title, i) => {
+            let regex = new RegExp(`(?<=^|[.'"\\s])${title}(?=[.'"\\s]|$)`, 'gi');
+            // NOTE: Needed to capitalise title in order to pass one of the freeCodeCamp tests.
+            let replacingTitle = titlesTo[i].charAt(0).toUpperCase() + titlesTo[i].slice(1);
+            text = text.replaceAll(regex, `<span class="highlight">${replacingTitle}</span>`);
+        });
+        
+        return text;
+    };
+
+    translateTime(text, dividerFrom, dividerTo) {
+        let regex = new RegExp(`(?<=^|[.'"\\s])(\\d{1,2})${dividerFrom}(\\d{2})(?=[.'"\\s]|$)`, 'g');
+        text = text.replaceAll(regex, `<span class="highlight">$1${dividerTo}$2</span>`);
+
+        return text;
+    }
+
+    translateToAmerican(text) {
+        text = this.translateWord(text, britishOnlyWords, americanFromBritishOnly);
+        text = this.translateWord(text, britishSpelling, americanSpelling);
+        text = this.translateTitle(text, britishTitles, americanTitles);
+        text = this.translateTime(text, '.', ':');
+
+        return text;
+    }
+    
+    translateToBritish(text) {
+        text = this.translateWord(text, americanOnlyWords, britishFromAmericanOnly);
+        text = this.translateWord(text, americanSpelling, britishSpelling);
+        text = this.translateTitle(text, americanTitles, britishTitles);
+        text = this.translateTime(text, ':', '.');
+
+        return text;
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,42 @@
 
 const Translator = require('../components/translator.js');
 
+const AMERICAN_TO_BRITISH_LOCALE = 'american-to-british';
+const BRITISH_TO_AMERICAN_LOCALE = 'british-to-american';
+
+const validLocales = [ AMERICAN_TO_BRITISH_LOCALE, BRITISH_TO_AMERICAN_LOCALE ];
+
 module.exports = function (app) {
   
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      console.log(req.body);
+
+      if(req.body.locale === undefined || req.body.text === undefined) {
+        res.json({ error: 'Required field(s) missing' });
+        return;
+      }
+
+      if(!req.body.text || req.body.text.trim().length === 0) {
+        res.json({ error: 'No text to translate' });
+        return;
+      }
+
+      if(!req.body.locale || !validLocales.includes(req.body.locale)) {
+        res.json({ error: 'Invalid value for locale field' });
+        return;
+      }
+
+      let translatedText = req.body.locale === AMERICAN_TO_BRITISH_LOCALE ?
+        translator.translateToBritish(req.body.text) :
+        translator.translateToAmerican(req.body.text);
+
+      if(translatedText === req.body.text) {
+        translatedText = 'Everything looks good to me!';
+      }
+
+      res.json({text: req.body.text, translation: translatedText});
     });
 };
